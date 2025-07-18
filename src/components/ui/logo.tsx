1cef7c5d2ef8beda0ef9f3e46045712fcc146763
Richard Villasencio <villasenciorichard@gmail.com>
2025-07-18 19:53:04 +0000

    Now I need you to exacle use this logo

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -43,28 +43,30 @@ export function TubsOfFunLogo(props: SVGProps<SVGSVGElement>) {
       <defs>
         <style>
           {`
-            .tubs-text { font-family: 'Poppins', sans-serif; font-size: 40px; font-weight: bold; fill: #e77931; }
-            .fun-text { font-family: 'Poppins', sans-serif; font-size: 40px; font-weight: bold; fill: #e77931; }
-            .ripple { fill: none; stroke: white; stroke-width: 2; }
+            .tubs-text { font-family: 'Poppins', sans-serif; font-size: 40px; font-weight: bold; }
+            .orange-text { fill: #e77931; }
+            .blue-text { fill: #0077c8; }
+            .ripple { fill: none; stroke: #ffffff; stroke-width: 2; }
           `}
         </style>
       </defs>
 
-      <text x="0" y="40" className="tubs-text">Tubs of</text>
-      <text x="155" y="40" className="fun-text">Fun!</text>
+      <text x="0" y="45" className="tubs-text orange-text">Tubs</text>
+      <text x="110" y="45" className="tubs-text blue-text">of</text>
+      <text x="160" y="45" className="tubs-text orange-text">Fun!</text>
       
-      <g transform="translate(180, 48)">
-        <circle className="ripple" r="5">
+      <g transform="translate(132, 48)">
+        <circle className="ripple" r="5" opacity="0.8">
           <animate attributeName="r" from="5" to="45" dur="2s" begin="0s" repeatCount="indefinite" />
-          <animate attributeName="opacity" from="1" to="0" dur="2s" begin="0s" repeatCount="indefinite" />
+          <animate attributeName="opacity" from="0.8" to="0" dur="2s" begin="0s" repeatCount="indefinite" />
         </circle>
-        <circle className="ripple" r="5">
+        <circle className="ripple" r="5" opacity="0.8">
           <animate attributeName="r" from="5" to="45" dur="2s" begin="0.5s" repeatCount="indefinite" />
-          <animate attributeName="opacity" from="1" to="0" dur="2s" begin="0.5s" repeatCount="indefinite" />
+          <animate attributeName="opacity" from="0.8" to="0" dur="2s" begin="0.5s" repeatCount="indefinite" />
         </circle>
-        <circle className="ripple" r="5">
+        <circle className="ripple" r="5" opacity="0.8">
           <animate attributeName="r" from="5" to="45" dur="2s" begin="1s" repeatCount="indefinite" />
-          <animate attributeName="opacity" from="1" to="0" dur="2s" begin="1s" repeatCount="indefinite" />
+          <animate attributeName="opacity" from="0.8" to="0" dur="2s" begin="1s" repeatCount="indefinite" />
         </circle>
       </g>
     </svg>
