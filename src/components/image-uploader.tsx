a5ca7431cc83617a7eec7aefe214ad6d6c4250c6
Richard Villasencio <villasenciorichard@gmail.com>
2025-07-19 21:09:20 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/image-uploader.tsx
+++ src/components/image-uploader.tsx
@@ -9,7 +9,7 @@ import {
   uploadBytesResumable,
   getDownloadURL,
 } from 'firebase/storage';
-import { Button } from '@/components/ui/button';
+import { Button, buttonVariants } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Progress } from '@/components/ui/progress';
