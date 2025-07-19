e019426be65e75c1cc8b912294116064b9f1eb57
Richard Villasencio <villasenciorichard@gmail.com>
2025-07-19 21:19:47 +0000

    STill the same, stuck in uploading

--- src/components/image-uploader.tsx
+++ src/components/image-uploader.tsx
@@ -1,7 +1,7 @@
 
 'use client';
 
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import Image from 'next/image';
 import { ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
 import { Button, buttonVariants } from '@/components/ui/button';
