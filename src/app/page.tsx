60cdc643f50b9622dfff1456d807813e05d0f4e8
Richard Villasencio <villasenciorichard@gmail.com>
2025-08-12 19:15:24 +0000

    I want you to adjust the backgroud video to be fit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -82,7 +82,7 @@ export default function HomePage() {
       
       {/* Swim Spa Master Section */}
        <section className="relative h-[700px] text-white flex items-center">
-        <video autoPlay loop muted playsInline className="absolute top-0 left-0 w-full h-full object-cover z-0" poster="https://placehold.co/1920x400.png">
+        <video autoPlay loop muted playsInline className="absolute top-0 left-0 w-full h-full object-contain z-0" poster="https://placehold.co/1920x400.png">
            <source src="https://firebasestorage.googleapis.com/v0/b/tubclone.firebasestorage.app/o/SwimPASBanner.mp4?alt=media&token=f8cb1b63-4f70-4347-b9f4-72fbff3eda0d" type="video/mp4" />
         </video>
         <div className="absolute top-0 left-0 w-full h-full z-10 bg-black/60"></div>
